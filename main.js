c35777871a01ca969e3088d5556b624751e1b35e
Ana clara <dev.anaclaramota@outlook.com>
2023-01-17 19:45:12 -0300

    commit-js

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 let display = document.getElementById('p')
+let seconds = 00
+let minutes = 00
+let hour = 00
+let interval 
 
+function menor10(num){
+    if(num < 10){
+        return('0'+ num)
 
-
+    }else{
+        return(num)
+    }
+}
 function start(){
-    alert('iniciou')
-    setInterval(counter,1000)
+    counter()
+  interval = setInterval(counter,1000)
 }
 
 function pause(){
-    alert('pausou')
+    clearInterval(interval)
 }
 function reset(){
-    alert('zerou')
+    clearInterval(interval)
+    seconds = 00
+    minutes = 00
+    hour = 00
+    display.innerText = '00:00:00'
 }
 
 function counter(){
-    display = display + 1 
-}
+    seconds++
+    if(seconds > 60){
+        minutes++
+        seconds = 00
+        if(minutes > 60){
+            hour++
+            minutes=00
+        }
+    }
+        display.innerText= menor10(hour)+':'+menor10(minutes)+':'+menor10(seconds)
+    }
+    
